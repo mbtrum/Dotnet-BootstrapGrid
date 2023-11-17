01e37cc732faffb5d82d1f55932fc109b2fa1bb7
W0304263 <michael.trumbull@nscc.ca>
2023-11-17 14:24:59 -0400

    Add project files.

---
+++ BootstrapGrid/Pages/Index.cshtml.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace BootstrapGrid.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ILogger<IndexModel> _logger;
+
+        public List<String> Dogs { get; set; } = new();
+
+        public IndexModel(ILogger<IndexModel> logger)
+        {
+            _logger = logger;
+
+            Dogs.Add("Charlie");
+            Dogs.Add("Max");
+            Dogs.Add("Cooper");
+            Dogs.Add("Milo");
+            Dogs.Add("Buddy");
+            Dogs.Add("Teddy");
+            Dogs.Add("Rocky");
+            Dogs.Add("Bear");
+            Dogs.Add("Leo");
+            Dogs.Add("Duk");
+            Dogs.Add("Luna");
+            Dogs.Add("Bella");
+            Dogs.Add("Daisy");
+            Dogs.Add("Lucy");
+            Dogs.Add("Lily");
+            Dogs.Add("Lola");
+            Dogs.Add("Zoe");
+            Dogs.Add("Sadie");
+            Dogs.Add("Stella");
+            Dogs.Add("Bailey");
+        }
+
+        public void OnGet()
+        {
+
+        }
+    }
+}
